2723d1c22cace903b6b1f02da1c605a0076c1862
Alexandru Szentpetery <Alex.Szentpetery@wkfs-frc.local>
2018-07-26 15:38:41 +0300

    Calculator inbunatatit dar incomplet

--- functions/src/main/java/ro/sci/functions/Main.java
+++ functions/src/main/java/ro/sci/functions/Main.java
@@ -4,16 +4,17 @@ import ro.sci.functions.math.Calculator;
 
 public class Main {
 
-    public static void main(String[]args){
-
-        System.out.println("salutare");
+    public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
 
-            System.out.println("suma:" + calculator.aduna(3,4));
+        System.out.println("suma:" + calculator.adunare(3, 4));
 
-        System.out.println("fibonacii:" + calculator.fibonacci(9) );
+        System.out.println("scadere:" + calculator.scadere (3, 4));
 
+        System.out.println("impartire:" + calculator.impartire (10, 2));
+
+        System.out.println("inmultire:" + calculator.inmultire(3, 4));
 
 
 
