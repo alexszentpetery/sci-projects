ccaf923ed481f5e3495494e846a841b079970f6f
Alexandru Szentpetery <Alex.Szentpetery@wkfs-frc.local>
2018-07-30 18:53:09 +0300

    .

--- zileAleLunii/src/main/java/ro/calendar/calculZile/CalculZile.java
+++ zileAleLunii/src/main/java/ro/calendar/calculZile/CalculZile.java
@@ -1,6 +1,6 @@
 package ro.calendar.calculZile;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
 /**
  * Clasa care calculeaza numarul de zile dintr-o luna
@@ -20,9 +20,9 @@ public class CalculZile {
 
     public int numarZile(String luna) {
 
-       luna = luna.toLowerCase();
-       String operatie;
-       operatie=luna;
+        luna = luna.toLowerCase();
+        String operatie;
+        operatie = luna;
 
         switch (luna) {
             case "ianuarie":
@@ -40,6 +40,7 @@ public class CalculZile {
             case "noiembrie":
                 numar = 30;
                 break;
+
             case "februarie":
                 String an = JOptionPane.showInputDialog(null, "introduceti anul;");
                 Double anSelectat = Double.parseDouble(an);
@@ -48,20 +49,20 @@ public class CalculZile {
                 //  se imparte la 100 - nu e an bisect
                 // se imparte la 400 -an bisect
 
-                if (anSelectat % 4 == 0 && (anSelectat %100 !=0 || anSelectat % 400==0)   ){
+                if (anSelectat % 4 == 0 && (anSelectat % 100 != 0 || anSelectat % 400 == 0)) {
                     numar = 29;
-            } else
-                 numar = 28;
-            break;
+                } else
+                    numar = 28;
+                break;
             default:
-                JOptionPane.showMessageDialog(null,"luna introdusa nu exista");
+                JOptionPane.showMessageDialog(null, "luna introdusa nu exista", "Error", JOptionPane.ERROR_MESSAGE);
 
-                Object[] possibilities = {"ianuarie", "februarie", "martie", "aprilie" , "mai" , "iunie" , "iulie","august","septembrie","octombrie","noiembrie","decembrie"};
+                Object[] possibilities = {"ianuarie", "februarie", "martie", "aprilie", "mai", "iunie", "iulie", "august", "septembrie", "octombrie", "noiembrie", "decembrie"};
                 operatie = (String) JOptionPane.showInputDialog(
                         null,
                         "Selectati o luna?",
                         "Selector Operatiune",
-                        JOptionPane.PLAIN_MESSAGE,
+                        JOptionPane.WARNING_MESSAGE,
                         null,
                         possibilities,
                         "ianuarie");
