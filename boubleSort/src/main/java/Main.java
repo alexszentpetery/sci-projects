e47cc7bcc3073da3190484d7dafc3fa94fc8ba18
Alex Szentpetery <alexszentpetery@yahoo.com>
2018-08-04 22:17:53 +0300

    merge cat de cat

--- boubleSort/src/main/java/Main.java
+++ boubleSort/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import sci.java.person.Person;
+import sci.java.person.PersonSorter;
 
 import javax.swing.*;
 
@@ -19,12 +20,14 @@ public class Main {
 
         for (int i = 0; i < Integer.parseInt(nrPersons); i++) {
             persoane[i] = new Person();
-            persoane[i].setFirstName(JOptionPane.showInputDialog(null, "Introduceti numele de familie P" + i));
-            persoane[i].setLastName(JOptionPane.showInputDialog(null, "Introduceti numele de botez P" + i));
+         //   persoane[i].setFirstName(JOptionPane.showInputDialog(null, "Introduceti numele de familie P" + i));
+         //   persoane[i].setLastName(JOptionPane.showInputDialog(null, "Introduceti numele de botez P" + i));
             varstaTemporar = JOptionPane.showInputDialog(null, "introduceti varsta P" + i);
             persoane[i].setAge(Integer.parseInt(varstaTemporar));
 
         }
+        PersonSorter sortare = new PersonSorter();
+                sortare.personSorter(persoane);
 /*
         for (int i = 0; i < Integer.parseInt(nrPersons); i++) {
 
--- boubleSort/src/main/java/sci/java/person/PersonSorter.java
+++ boubleSort/src/main/java/sci/java/person/PersonSorter.java
@@ -2,10 +2,32 @@ package sci.java.person;
 
 public class PersonSorter {
 
-    public void PersonSorter(){
+    public void personSorter(Person[] sortarePersoane) {
+        int tempAge;
+        int tempAge2;
+        boolean flag;
+        flag=true;
 
+        while (flag){
+            flag=false;
 
-
+        for (int i = 0; i < sortarePersoane.length -1; i++) {
+            Person forSwapPerson = sortarePersoane[i];
+            tempAge = sortarePersoane[i].getAge();
+            tempAge2 = sortarePersoane[i + 1].getAge();
+            if (tempAge > tempAge2) {
+                sortarePersoane[i] = sortarePersoane[i + 1];
+                sortarePersoane[i + 1] = forSwapPerson;
+                flag=true;
+            }
         }
+        }
+        for (int i = 0; i < sortarePersoane.length; i++) {
+        //    System.out.printf("" + sortarePersoane[i].getFirstName());
+        //    System.out.printf("" + sortarePersoane[i].getLastName());
+            System.out.println("" + sortarePersoane[i].getAge());
+        }
+
+    }
 
 }
