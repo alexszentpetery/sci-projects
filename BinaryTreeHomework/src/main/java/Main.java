9ec0c1c08ab2285a5de32e2d234f874e51dce4f5
Alex Szentpetery <alexszentpetery@yahoo.com>
2018-09-27 10:38:58 +0300

    prima versiune , mai mult ca sigur nu e bine...

---
+++ BinaryTreeHomework/src/main/java/Main.java
@@ -0,0 +1,45 @@
+import binary.tree.BinaryTree;
+import binary.tree.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main extends Thread {
+
+    public static void main(String[] args) {
+        BinaryTree lista = new BinaryTree();
+
+        lista.add("f");
+        lista.add("d");
+        lista.add("b");
+        lista.add("a");
+        lista.add("t");
+        lista.add("u");
+        lista.add("m");
+        lista.add("n");
+        lista.add("e");
+        lista.add("g");
+        lista.add("r");
+        lista.add("o");
+
+//
+//        lista.add(5);
+//        lista.add(12);
+//        lista.add(3);
+//        lista.add(1);
+//        lista.add(11);
+//        lista.add(9);
+//        lista.add(29);
+//        lista.add(7);
+//        lista.add(50);
+//        lista.add(18);
+//        lista.add(33);
+//            lista.add(4);
+        lista.createNodeList();
+///
+
+
+
+    }
+
+}
---
+++ BinaryTreeHomework/src/main/java/binary/tree/BinaryTree.java
@@ -0,0 +1,69 @@
+package binary.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BinaryTree<E extends Comparable<E>> implements Comparable<E> {
+    List<Node> nodeList = new ArrayList<Node>();
+    private Node nod;
+    private E value;
+    private List<E> list = new ArrayList<E>();
+
+    public BinaryTree() {
+    }
+
+    public void add(E i) {
+        list.add(i);
+    }
+
+    public void createNodeList() {
+        Node rootNod = new Node(list.get(0));
+        nodeList.add(rootNod);
+        for (int i = 1; i < list.size(); i++) {
+            createNode(list.get(i), rootNod);
+        }
+        for (Node nodes : nodeList) {
+            nodes.printNode();
+            //      System.out.println(nodes);
+
+        }
+    }
+
+    // we recevei root node and the first intem from the list
+    private <E extends Comparable> void createNode(E integer, Node givenNode) {
+
+        // if value from list is bigger than root
+        if (integer.compareTo(givenNode.getValue()) > 0) {
+            //if root right possition is empty then we can create a node and set the right position of the initial node
+            if (givenNode.getRightValue() == null) {
+                nod = new Node(integer);
+                givenNode.setRightValue(nod);
+                nodeList.add(nod);
+            } else {
+                //if the right side of the initial node is not empty , we call again this function but changing root = right side node
+                createNode(integer, givenNode.getRightValue());
+            }
+        }
+        // if value from list is bigger than root
+        if (integer.compareTo(givenNode.getValue()) < 0) {
+            //if root left possition is empty then we can create a node and set the left position of the initial node
+            if (givenNode.getLeftValue() == null) {
+                nod = new Node(integer);
+                givenNode.setLeftValue(nod);
+                nodeList.add(nod);
+            }
+            //if the left side of the initial node is not empty , we call again this function but changing root = side side node
+            else {
+                createNode(integer, givenNode.getLeftValue());
+            }
+        }
+    }
+
+    public int compareTo(E o) {
+        return nod.getValue().compareTo(o);
+    }
+
+
+}
+
